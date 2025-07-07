839ac40197d1482add8cd6b323bc76c06dd31218
sridharn96 <sridharnaagarajan@gmail.com>
2025-07-07 09:32:42 +0530

    title

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -30,6 +30,7 @@ const LoginPage = () => {
       {/* -------- left -------- */}
       <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
 
+  
       {/* -------- right -------- */}
       <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
         <h2 className='font-medium text-2xl flex justify-between items-center'>
